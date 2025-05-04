105067e8ca7dc26e0e7c73efe05f221446063511
Mariusz N <mnowak02@wp.pl>
2025-05-04 17:34:21 +0200

    added stage

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,13 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'AwesomePipeline',{
+    const pipeline = new CodePipeline(this, 'AwesomePipeline',{
       pipelineName: "YouAreAwsome",
       synth: new ShellStep('Synth',{
         input: CodePipelineSource.gitHub('mnowak02/cicd-pipeline-alexdan','cicd-practice1'),
@@ -18,7 +19,11 @@ export class CdkCicdStack extends cdk.Stack {
         ]
       })
 
-    })
+    });
+
+    const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage',{
+      stageName = 'stagename-test'
+    }))
 
 
   }

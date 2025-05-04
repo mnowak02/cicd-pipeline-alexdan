7de204ba3a99ed63ec7ec086d726ca6316eb4f71
Mariusz N <mnowak02@wp.pl>
2025-05-04 16:13:17 +0200

    added ls

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,10 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'AwesomePipeline',{
+      pipelineName: "YouAreAwsome",
+      synth: new ShellStep('Synth',{
+        input: CodePipelineSource.gitHub('mnowak02/cicd-pipeline-alexdan','cicd-practice1'),
+        commands: [
+          'npm ci',
+          'npx cdk synth',
+          'ls -al'
+        ]
+      })
+
+    })
 
 
   }
